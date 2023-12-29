c7fef5c6164b73ce965dc1dfa53972e5f31b249b
Ranjan Malav <ranjan2192@gmail.com>
2023-12-29 17:44:50 +0530

    Update UI.

--- src/ReactApp.js
+++ src/ReactApp.js
@@ -1,35 +1,60 @@
-import * as React from 'react';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import './App.css';
-import { Alert, Button, Snackbar, TextField } from '@mui/material';
-import { LoadingButton } from '@mui/lab';
-import { collections } from './schema';
+import * as React from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import "./App.css";
+import { Alert, Button, Snackbar, TextField } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { collections } from "./schema";
 
-const dataView = (keys, data) => (
-  data ? (<Box style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', textAlign: 'start' }}>
-    {
-      keys.map(key =>
-        <Typography style={{ color: 'black' }} key={key}>{`${key}: ${JSON.stringify(data[key])}`}</Typography>
-      )
-    }
-  </Box>) : null
-)
+const dataView = (keys, data) =>
+  data ? (
+    <Box
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        textAlign: "start",
+        borderColor: "black",
+        maxHeight: "500px",
+        border: "1px solid black",
+        padding: "15px",
+      }}
+    >
+      {keys.map((key) => (
+        <Typography
+          style={{ color: "black" }}
+          key={key}
+        >{`${key}: ${JSON.stringify(data[key])}`}</Typography>
+      ))}
+    </Box>
+  ) : null;
 
 function CustomTabPanel(props) {
-  const { activeIndex, index, data, searchData, loading, dataKey, path, onClick, ...other } = props;
+  const {
+    activeIndex,
+    index,
+    data,
+    searchData,
+    loading,
+    dataKey,
+    path,
+    onClick,
+    ...other
+  } = props;
   const collectionPath = dataKey ? dataKey : path;
 
-  const dataKeys = collections.find((element) => element.key === collectionPath || element.path === path).props
+  const dataKeys = collections.find(
+    (element) => element.key === collectionPath || element.path === path
+  ).props;
 
   const [search, setSearch] = React.useState(searchData);
 
   const [openSnackbar, setOpenSnackbar] = React.useState(false);
 
   const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
 
@@ -45,46 +70,69 @@ function CustomTabPanel(props) {
       {...other}
     >
       {activeIndex === index && (
-        <Box sx={{ p: 3 }} >
-          <Box style={{ display: 'flex' }}>
+        <Box sx={{ p: 3 }}>
+          <Box style={{ display: "flex" }}>
             <TextField
               id="outlined-basic"
               label={dataKey ? "Document path" : "Document ID"}
               variant="outlined"
               value={search}
-              onChange={e => setSearch(e.target.value)}
-              style={{ minWidth: '300px' }}
+              onChange={(e) => setSearch(e.target.value)}
+              style={{ width: "300px" }}
             />
             <LoadingButton
               variant="contained"
-              style={{ marginLeft: '10px' }}
+              style={{ marginLeft: "10px" }}
               loading={loading}
               onClick={() => {
-                if (!search) return
-                const urlPath = dataKey ? `custom?path=${search}` : `${path}/${search}`
-                onClick(urlPath, collectionPath, search)
-              }}>
+                if (!search) return;
+                const urlPath = dataKey
+                  ? `custom?path=${search}`
+                  : `${path}/${search}`;
+                onClick(urlPath, collectionPath, search);
+              }}
+            >
               Search
             </LoadingButton>
           </Box>
-          {data ? (<Box style={{ display: 'flex', marginTop: '10px' }}>
-            <Button
-              variant="contained"
-              onClick={() => {
-                navigator.clipboard.writeText(JSON.stringify(data))
-                setOpenSnackbar(true)
-              }}>
-              Copy
-            </Button>
-            <div style={{ marginLeft: '10px' }}>
-              {dataView(dataKeys, data)}
-            </div>
-            <Snackbar open={openSnackbar} autoHideDuration={2000} onClose={handleClose}>
-              <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
-                Copied to Clipboard!
-              </Alert>
-            </Snackbar>
-          </Box>) : null}
+          {data ? (
+            <Box
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                marginTop: "10px",
+                alignItems: "flex-start",
+              }}
+            >
+              <Typography style={{ color: "black" }}>Result</Typography>
+              <Box style={{ display: "flex", alignItems: "flex-start" }}>
+                <div style={{ width: "300px" }}>{dataView(dataKeys, data)}</div>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    navigator.clipboard.writeText(JSON.stringify(data));
+                    setOpenSnackbar(true);
+                  }}
+                  style={{ marginLeft: "10px" }}
+                >
+                  Copy JSON
+                </Button>
+              </Box>
+              <Snackbar
+                open={openSnackbar}
+                autoHideDuration={2000}
+                onClose={handleClose}
+              >
+                <Alert
+                  onClose={handleClose}
+                  severity="success"
+                  sx={{ width: "100%" }}
+                >
+                  Copied to Clipboard!
+                </Alert>
+              </Snackbar>
+            </Box>
+          ) : null}
         </Box>
       )}
     </div>
@@ -94,7 +142,7 @@ function CustomTabPanel(props) {
 function TabProps(index) {
   return {
     id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
   };
 }
 
@@ -109,48 +157,64 @@ function ReactApp() {
   };
 
   /**
-   * Fetches data from the API, 
+   * Fetches data from the API,
    * urlPath is attached to the base API, e.g. users/xyz or users/xyz/roles/123
    * collectionPath is used to keep store data, e.g. users
-   * @param {string} urlPath 
-   * @param {string} collectionPath 
+   * @param {string} urlPath
+   * @param {string} collectionPath
    */
   function getData(urlPath, collectionPath, searchQuery) {
-    setLoading({ ...loading, [collectionPath]: true })
+    setLoading({ ...loading, [collectionPath]: true });
     fetch(`http://localhost:3000/${urlPath}`)
       .then(async (response) => {
         var body = await response.json();
-        console.log(JSON.stringify(body))
-        setData({ ...data, [collectionPath]: body })
-        setSearchData({ ...searchData, [collectionPath]: searchQuery })
-        setLoading({ ...loading, [collectionPath]: false })
+        console.log(JSON.stringify(body));
+        setData({ ...data, [collectionPath]: body });
+        setSearchData({ ...searchData, [collectionPath]: searchQuery });
+        setLoading({ ...loading, [collectionPath]: false });
       })
-      .catch(err => {
-        console.log(err)
-        setLoading({ ...loading, [collectionPath]: false })
-        alert(err)
-      })
+      .catch((err) => {
+        console.log(err);
+        setLoading({ ...loading, [collectionPath]: false });
+        alert(err);
+      });
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <Box sx={{ width: '100%', flex: 1 }}>
-          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Box sx={{ width: "100%", flex: 1 }}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
             <Tabs value={activeIndex} onChange={handleChange} aria-label="tabs">
-              {collections.map((collection, index) => <Tab label={collection.label} {...TabProps(index)} />)}
+              {collections.map((collection, index) => (
+                <Tab label={collection.label} {...TabProps(index)} />
+              ))}
             </Tabs>
           </Box>
           {collections.map((collection, index) => (
             <CustomTabPanel
               activeIndex={activeIndex}
               index={index}
-              data={collection.path === 'custom' ? data[collection.key] : data[collection.path]}
-              searchData={collection.path === 'custom' ? searchData[collection.key] : searchData[collection.path]}
-              loading={collection.path === 'custom' ? loading[collection.key] : loading[collection.path]}
+              data={
+                collection.path === "custom"
+                  ? data[collection.key]
+                  : data[collection.path]
+              }
+              searchData={
+                collection.path === "custom"
+                  ? searchData[collection.key]
+                  : searchData[collection.path]
+              }
+              loading={
+                collection.path === "custom"
+                  ? loading[collection.key]
+                  : loading[collection.path]
+              }
               dataKey={collection.key}
               path={collection.path}
-              onClick={(urlPath, collectionPath, searchQuery) => getData(urlPath, collectionPath, searchQuery)}
+              onClick={(urlPath, collectionPath, searchQuery) =>
+                getData(urlPath, collectionPath, searchQuery)
+              }
             />
           ))}
         </Box>
